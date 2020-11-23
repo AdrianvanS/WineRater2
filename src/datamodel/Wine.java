762e2d71eba392c991c021d1c58d697ad4f59bc1
Adrian_vSk <agvsmail@gmail.com>
2020-11-23 19:53:22 +0000

    Created body of UI and WineList class to hold wines

--- src/datamodel/Wine.java
+++ src/datamodel/Wine.java
@@ -18,4 +18,102 @@ public class Wine {
     //Default constructor for when loading the data
     public Wine() {
     }
+
+    public Wine(SimpleStringProperty name, SimpleStringProperty color, SimpleIntegerProperty year,
+                SimpleStringProperty producer, SimpleStringProperty variety, SimpleIntegerProperty rating,
+                SimpleDoubleProperty price) {
+        this.name = name;
+        this.color = color;
+        this.year = year;
+        this.producer = producer;
+        this.variety = variety;
+        this.rating = rating;
+        this.price = price;
+    }
+
+    //Getters and Setter
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getColor() {
+        return color.get();
+    }
+
+    public SimpleStringProperty colorProperty() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color.set(color);
+    }
+
+    public int getYear() {
+        return year.get();
+    }
+
+    public SimpleIntegerProperty yearProperty() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year.set(year);
+    }
+
+    public String getProducer() {
+        return producer.get();
+    }
+
+    public SimpleStringProperty producerProperty() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer.set(producer);
+    }
+
+    public String getVariety() {
+        return variety.get();
+    }
+
+    public SimpleStringProperty varietyProperty() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety.set(variety);
+    }
+
+    public int getRating() {
+        return rating.get();
+    }
+
+    public SimpleIntegerProperty ratingProperty() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating.set(rating);
+    }
+
+    public double getPrice() {
+        return price.get();
+    }
+
+    public SimpleDoubleProperty priceProperty() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price.set(price);
+    }
 }
--- src/datamodel/WineList.java
+++ src/datamodel/WineList.java
@@ -1,4 +1,30 @@
 package datamodel;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class WineList {
+
+    private ObservableList<Wine> wines;
+
+    //Initialize the wines list
+    public WineList(){
+        wines = FXCollections.observableArrayList();
+    }
+
+    //Access wines
+    public ObservableList<Wine> getWines(){
+        return wines;
+    }
+
+    //Add a wine
+    public void addWine(Wine wine){
+        wines.add(wine);
+    }
+
+    //Remove a wine
+    public void deleteWine(Wine wine){
+        wines.remove(wine);
+    }
+
 }
